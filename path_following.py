d49769ec764676ffed7d4d5f26a32e72841f7b9d
Renato Pacheco Vieira <renato.pacheco.r2@gmail.com>
2016-07-05 13:47:01 -0400

    Alterando PathFollowing para usar for

--- path_following.py
+++ path_following.py
@@ -12,14 +12,11 @@ class PathFollowing(Sordalane):
         self.points = points
 
     def run(self):
-        i = 0
-        print i, len(self.points)
-        (self.x_p, self.y_p, self.theta_p) = self.points[i]        
-        while not rospy.is_shutdown():
-            self.refresh_position()
-            self.publish_velocities()
+        for p in self.points:
+            (self.x_p, self.y_p, self.theta_p) = self.points[i]        
+            while not rospy.is_shutdown():
+                self.refresh_position()
+                self.publish_velocities()
 
-            if len(self.points) > i and (fabs(self.err_x) < 0.2 and fabs(self.err_y) < 0.2):
-                i+=1
-                print self.points[i], self.err_x, self.err_y
-                (self.x_p, self.y_p, self.theta_p) = self.points[i]
+            if fabs(self.err_x) < 0.02 and fabs(self.err_y) < 0.02:
+                break
